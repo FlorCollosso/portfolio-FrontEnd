07b581ccdefc0f7faffeb9e85b238e15e47849b1
FlorCollosso <florcollosso@gmail.com>
2022-11-09 00:18:37 -0300

    Responsive percentage skills

--- Portfolio/src/app/components/percentages-skills/percentages-skills.component.ts
+++ Portfolio/src/app/components/percentages-skills/percentages-skills.component.ts
@@ -10,6 +10,27 @@ export class PercentagesSkillsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
+    if(window.innerWidth <= 1880) {
 
+      const ratings = Array.from(document.getElementsByClassName('rating'));
+
+      ratings.forEach((rating) => { 
+        const ratingContent = rating.innerHTML;
+        const ratingScore = parseInt(ratingContent, 10);
+
+         // Define if the score is good, meh or bad according to its value
+         
+        const scoreClass = ratingScore < 40 ? "bad" : ratingScore < 60 ? "meh" : "good";
+
+        // Add score class to the rating
+
+        rating.classList.add(scoreClass);
+
+        const ratingColor = window.getComputedStyle(rating).backgroundColor;
+        const gradient = `background: conic-gradient(${ratingColor} ${ratingScore}%, transparent 0 100%)`;
+
+        rating.setAttribute("style", gradient);
+      });
+    } 
+  }
 }
